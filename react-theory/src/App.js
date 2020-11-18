d706a995c5eda8c0e32d536e948da20b4c5910cf
svdfsdev <svd.fsdev@gmail.com>
2020-11-18 17:33:25 +0300

    feat: add updating title

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,23 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import Car from './Car/Car';
 
-const App = () => {
-  const appStyle = {
-    div: {
-      textAlign: 'center',
-    },
-    h1: {
-      color: 'blue',
-    },
+class App extends Component {
+  state = {
+    cars: [
+      { name: 'Ford', year: 2018 },
+      { name: 'Audi', year: 2016 },
+      { name: 'Mazda', year: 2010 },
+    ],
+    pageTitle: 'React components',
+    counter: 0,
   };
 
-  return (
-    <div style={appStyle.div}>
-      <h1 style={appStyle.h1}>Hello World!</h1>
-      <Car />
-    </div>
-  );
-};
+  changeTitleHandler = (newTitle) => {
+    this.setState({
+      pageTitle: newTitle,
+    });
+  };
+
+  handleInput = (event) => {
+    this.setState({
+      pageTitle: event.target.value,
+    });
+  };
+
+  render() {
+    const appStyle = {
+      div: {
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        width: '50%',
+        height: '100vh',
+        margin: '0 auto',
+      },
+      h1: {
+        color: 'blue',
+      },
+    };
+
+    const inputValue = React.createRef();
+    const cars = this.state.cars;
+
+    return (
+      <div style={appStyle.div}>
+        <h1 style={appStyle.h1}>{this.state.pageTitle}</h1>
+        <div>
+          <input
+            type="text"
+            ref={inputValue}
+            onChange={this.handleInput}
+          />
+          <button
+            onClick={this.changeTitleHandler.bind(this, 'Home page')}
+          >
+            Change title
+          </button>
+        </div>
+        <Car
+          name={cars[0].name}
+          year={cars[0].year}
+          onChangeTitle={this.changeTitleHandler}
+        />
+        <Car
+          name={cars[1].name}
+          year={cars[1].year}
+          onChangeTitle={this.changeTitleHandler}
+        />
+        <Car
+          name={cars[2].name}
+          year={cars[2].year}
+          onChangeTitle={this.changeTitleHandler}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- react-theory/src/Car/Car.jsx
+++ react-theory/src/Car/Car.jsx
@@ -1,8 +1,24 @@
-const Car = () => {
+const Car = (props) => {
+  const { name, year, onChangeTitle } = props;
+  const style = {
+    car: {
+      border: '1px solid grey',
+      borderRadius: '6px',
+      width: '150px',
+      padding: '20px',
+      margin: '0 auto',
+    },
+  };
+
   return (
-    <div>
-      This is a car component
-      <strong>test</strong>
+    <div style={style.car}>
+      <p>
+        Car name: <strong>{name}</strong>
+      </p>
+      <p>
+        Year: <strong>{year}</strong>
+      </p>
+      <button onClick={onChangeTitle.bind(this, name)}>Click</button>
     </div>
   );
 };
