23aeef7b3ddef949c3c6015aa3a824fa5767769c
svdfsdev <svd.fsdev@gmail.com>
2020-11-18 13:31:39 +0300

    feat: add Car component

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Car from './Car/Car';
 
-function App() {
+const App = () => {
+  const appStyle = {
+    div: {
+      textAlign: 'center',
+    },
+    h1: {
+      color: 'blue',
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={appStyle.div}>
+      <h1 style={appStyle.h1}>Hello World!</h1>
+      <Car />
     </div>
   );
-}
+};
 
 export default App;
---
+++ react-theory/src/Car/Car.jsx
@@ -0,0 +1,10 @@
+const Car = () => {
+  return (
+    <div>
+      This is a car component
+      <strong>test</strong>
+    </div>
+  );
+};
+
+export default Car;
