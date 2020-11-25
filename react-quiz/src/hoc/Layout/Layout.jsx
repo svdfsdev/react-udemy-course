88d6c962c374a3e43650df740da268b6bbe1f940
svdfsdev <svd.fsdev@gmail.com>
2020-11-25 15:02:10 +0300

    feat: add toogler menu

--- react-quiz/src/hoc/Layout/Layout.jsx
+++ react-quiz/src/hoc/Layout/Layout.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import MenuToogle from '../../components/Navigation/MenuToogle/MenuToogle';
 import classes from './Layout.module.scss';
 
 class Layout extends Component {
+  state = {
+    menu: false,
+  };
+
+  toogleMenuHandler = () => {
+    this.setState({ menu: !this.state.menu });
+  };
+
   render() {
     return (
       <div className={classes.Layout}>
+        <MenuToogle
+          onToogle={this.toogleMenuHandler}
+          isOpen={this.state.menu}
+        />
         <main>{this.props.children}</main>
       </div>
     );
