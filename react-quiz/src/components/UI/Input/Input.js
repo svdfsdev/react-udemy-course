b5c6bef744d46720da4b84b5b3b405a15b210401
svdfsdev <svd.fsdev@gmail.com>
2020-12-11 17:28:32 +0300

    feat: firebase deploy

--- react-quiz/src/components/UI/Input/Input.js
+++ react-quiz/src/components/UI/Input/Input.js
@@ -18,6 +18,7 @@ const Input = props => {
     <div className={cls.join(' ')}>
       <label htmlFor={htmlFor}>{props.label}</label>
       <input
+      
         type={inputType}
         id={htmlFor}
         value={props.value}
